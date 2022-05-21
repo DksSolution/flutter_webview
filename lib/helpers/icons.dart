36804d6dbe32d56a7f49b136585e42874589c5f2
D_K <dhrumilkhokhani007@gmail.com>
2022-05-21 22:35:59 +0530

    Done for Android Now check for iOS

---
+++ lib/helpers/icons.dart
@@ -0,0 +1,8 @@
+import '../helpers/Constant.dart';
+
+import 'package:flutter/material.dart';
+
+extension CustomIcons on ColorScheme {
+
+  String get noInternetIcon => 'assets/no_internet.svg';
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../widget/no_internet.dart';
+import '../widget/no_internet_widget.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,8 +20,15 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late String URL;
+  bool isLoading=true;
   late WebViewController _controller;
-  bool device = false;
+
+
+
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   @override
   void initState(){
     if(Platform.isAndroid){
@@ -25,36 +39,115 @@ class _HomeState extends State<Home> {
       // _controller.loadUrl(URL);
     }
     super.initState();
+    NoInternet.initConnectivity().then((value) => setState(() {
+      _connectionStatus = value;
+    }));
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+          NoInternet.updateConnectionStatus(result).then((value) => setState(() {
+            _connectionStatus = value;
+          }));
+        });
   }
-
-  bool _checkDevice(){
-    if(Platform.isAndroid){
+  willPopScope() async {
+    bool goBack = _controller.canGoBack() as bool;
+    if(goBack != true) {
       return true;
-    }else{
+    } else {
+      _controller.goBack();
       return false;
     }
   }
 
-  void _changeURL(){
-    if(Platform.isAndroid){
-      URL = 'https://p4del.app/?ref=android';
-      _controller.loadUrl(URL);
-    }else{
-      URL = 'https://p4del.app/?ref=apple';
-      _controller.loadUrl(URL);
-    }
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
   }
+  final Completer<WebViewController> _controllerCompleter =
+  Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     print(URL);
+    print(_connectionStatus);
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(0, 38, 0, 0),
-        child: WebView(
-          initialUrl:  URL,
-          javascriptMode: JavascriptMode.unrestricted,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            _connectionStatus == 'ConnectivityResult.none'
+                ? const NoInternetWidget()
+            :
+            Container(
+              child: WillPopScope(
+                onWillPop: () => _goBack(context),
+                child: WebView(
+                  initialUrl:  URL,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  onPageFinished: (finish){
+                    setState((){
+                      isLoading = false;
+                    });
+                  },
+                  onWebViewCreated: (WebViewController webViewController) {
+
+                    _controllerCompleter.future.then((value) => _controller = value);
+                    _controllerCompleter.complete(webViewController);
+                  },
+                  navigationDelegate: (NavigationRequest request) {
+                    if (request.url.startsWith('https://p4del.app/')) {
+                      print('allowing navigation to $request');
+                      return NavigationDecision.navigate;
+                    } else  {
+                      print('Opening Default Browser $request');
+                      _launchURL(request.url); // to open browser
+                      return NavigationDecision.prevent;
+                    }
+                  },
+                ),
+              ),
+            ),
+            isLoading ? const Center( child: CircularProgressIndicator(color: Color(0xFF25d366)),)
+                : Stack(),
+          ],
         ),
       ),
     );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        webOnlyWindowName: '_blank',
+      );
+    } else {
+      await launch(
+        url,
+        webOnlyWindowName: '_blank',
+      );
+    }
+
+  }
+  Future<bool> _goBack(BuildContext context) async {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Do you want to exit'),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('No'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  SystemNavigator.pop();
+                },
+                child: Text('Yes'),
+              ),
+            ],
+          ));
+      return Future.value(true);
   }
 }
